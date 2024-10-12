2fb43ddceb2255bfd6ec66bcf9cfb86b8909d314
Codie Newark <a13x231mcsl@gmail.com>
2024-10-12 18:58:44 +0100

    laptop changes.

--- blf_lib/src/io/packed_encoding.rs
+++ blf_lib/src/io/packed_encoding.rs
@@ -1,12 +1,15 @@
+use std::mem;
+use std::ops::{Rem, Sub};
 use crate::io::endian::Endianness;
 use crate::io::packing::Packing;
 
 pub trait PackedEncoder {
-    fn encode_packed(&self, endian: Endianness, packing: Packing, position: usize) -> Vec<u8>;
+    fn encode_packed(&self, endian: Endianness, packing: Packing) -> Vec<u8>;
 }
 
-// impl PackedEncoder for u32 {
-//     fn encode_packed(&self, endian: Endianness, packing: Packing, position: usize) -> Vec<u8> {
-//         Vec<>
-//     }
-// }
+impl PackedEncoder for u32 {
+    fn encode_packed(&self, endian: Endianness, packing: Packing) -> Vec<u8> {
+        let encoded_length = mem::size_of::<u32>();
+        let padding_size = (packing - (encoded_length % packing)) % packing;
+    }
+}
--- blf_lib/src/io/packing.rs
+++ blf_lib/src/io/packing.rs
@@ -1,5 +1,8 @@
+use std::ops::{Rem, Sub};
+
+#[derive(Clone, Copy)]
 pub struct Packing {
-    packing: u8
+    packing: usize
 }
 
 pub const PACK1: Packing = Packing { packing: 1 };
@@ -8,7 +11,7 @@ pub const PACK4: Packing = Packing { packing: 4 };
 pub const PACK8: Packing = Packing { packing: 8 };
 pub const PACK16: Packing = Packing { packing: 16 };
 
-const valid_packing_values: [u8; 5] = [
+const valid_packing_values: [usize; 5] = [
     1,
     2,
     4,
@@ -17,10 +20,35 @@ const valid_packing_values: [u8; 5] = [
 ];
 
 impl Packing {
-    pub fn new(packing: u8) -> Self {
+    pub fn new(packing: usize) -> Self {
         if !valid_packing_values.contains(&packing) {
             panic!("Invalid pack value: {packing}");
         }
         Packing { packing }
+    }
+}
+
+impl Rem for Packing {
+    type Output = ();
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        self.packing % rhs.packing;
+    }
+}
+
+
+impl Rem<Packing> for usize {
+    type Output = ();
+
+    fn rem(self, rhs: Packing) -> Self::Output {
+        self % rhs.packing;
+    }
+}
+
+impl Sub<()> for Packing {
+    type Output = ();
+
+    fn sub(self, rhs: ()) -> Self::Output {
+        todo!()
     }
 }
